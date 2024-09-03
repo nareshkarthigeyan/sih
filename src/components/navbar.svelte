0b5201aedea0a857dfc6d33417ccc8b7be59b013
K V Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-09-03 21:14:37 +0530

    navbar

---
+++ src/components/navbar.svelte
@@ -0,0 +1,46 @@
+<script>
+    import NavbarItem from "./navbarItem.svelte";
+    let items = ["Dashboard", "Student", "Staff", "Academics", "Performance", "Fees"];
+</script>
+
+<div class="app-container">
+    <nav class="navbar">
+        {#each items as item}
+            <NavbarItem name={item} />
+        {/each}
+    </nav>
+    <div class="content">
+        main content
+    </div>
+</div>
+
+<style>
+    .app-container {
+        display: flex;
+        height: 100vh;
+    }
+
+    .navbar {
+        background-color: #DDDEEE;
+        width: 20%;
+        padding: 20px;
+        box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
+        display: flex;
+        flex-direction: column;
+    }
+
+    .navbar p {
+        margin: 0;
+    }
+
+    .content {
+        width: 80%;
+        padding: 20px;
+        background-color: #F9F9F9;
+    }
+
+    .navbar .btn-active {
+        background-color: #9E7BB5;
+        color: white;
+    }
+</style>
---
+++ src/components/navbarItem.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let name;
+    export let redirectTo = "https://www.google.com"; // Full URL with protocol
+</script>
+
+<div class="btn" on:click={() => { window.location.href = redirectTo }}>
+    {name}
+</div>
+
+
+<style>
+    .btn {
+        padding: 15px;
+        width: 100%;
+        color: black;
+        border-radius: 5px;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        display: flex;
+        align-items: center;
+        margin: 5px 0;
+        transition: background-color 0.3s;
+    }
+
+    .btn:text {
+        margin-left: 10px;
+    }
+
+    .btn:hover {
+        background-color: #A492B5;
+        cursor: pointer;
+    }
+
+    .btn:active {
+        background-color: #A492B5;
+        color: white;
+    }
+</style>
